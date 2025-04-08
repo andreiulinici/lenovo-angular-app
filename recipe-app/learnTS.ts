1f5b3f890e0efe541f70e666b6a2faab8bddf74a
Andrei Ulinici <aulinici@lenovo.com>
2025-04-08 13:04:46 +0300

    Add recipes

---
+++ recipe-app/learnTS.ts
@@ -0,0 +1,3 @@
+let hi: string = 'Hello';
+console.log(hi);
+
---
+++ recipe-app/src/app/services/recipes.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from '../interfaces/recipe.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipesService {
+  recipes: Recipe[] = [
+    {
+      id: '1',
+      name: 'Classic Margherita Pizza',
+      image: 'https://cdn.dummyjson.com/recipe-images/1.webp',
+      tags: ['Pizza', 'Italian'],
+      prepTimeMinutes: 20,
+    },
+  ];
+
+  constructor() {}
+}
