fb389d2faf63209e7177de2d5553e5a0ebd323bc
madam2 <madam2@lenovo.com>
2025-04-29 10:50:17 +0300

    session-3-angular-partial

--- recipe-app/src/app/pages/home/home.component.ts
+++ recipe-app/src/app/pages/home/home.component.ts
@@ -2,17 +2,20 @@ import { Component } from '@angular/core';
 import { RecipeCardComponent } from '../../components/recipe-card/recipe-card.component';
 import { Recipe } from '../../interfaces/recipe.interface';
 import { RecipesService } from '../../services/recipes.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [RecipeCardComponent],
+  imports: [RecipeCardComponent, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   recipes: Recipe[] = [];
   dummyRecipes!: Recipe[];
+  filteredRecipes!: Recipe[];
   errorMessage: any = '';
+  searchValue = '';
 
   constructor(recipesService: RecipesService) {
     this.recipes = recipesService.recipes;
@@ -21,6 +24,7 @@ export class HomeComponent {
         next: (response) => {
           console.log(response);
           this.dummyRecipes = response.recipes;
+          this.filteredRecipes = response.recipes;
         },
         error: (err) => {
           console.log(err);
@@ -30,5 +34,11 @@ export class HomeComponent {
     } catch (error) {
       this.errorMessage = error;
     }
+  }
+
+  filterValues() {
+    this.filteredRecipes = this.dummyRecipes.filter((recipe) =>
+      recipe.name.toUpperCase().includes(this.searchValue.toUpperCase())
+    );
   }
 }
